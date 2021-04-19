033ee4913f84b0359998086c12a8362f77e87d2c
parthb83 <parthbansal1536@gmail.com>
2021-04-19 23:48:12 +0530

    Changed Double player algo

--- app2.js
+++ app2.js
@@ -14,10 +14,12 @@ const winnerComb = [
 ];
 
 const boxes = document.querySelectorAll(".box");
+document.querySelector(".turn").innerText = "Player-1 Turn";
 startGame();
 
 function startGame() {
   chance = 0;
+  document.querySelector(".turn").innerText = "Player-1 Turn";
   document.querySelector(".endgame").style.display = "none";
   boardDesc = Array.from(Array(9).keys());
   for (let i = 0; i < boxes.length; i++) {
@@ -29,8 +31,13 @@ function startGame() {
 
 function boxClick(square) {
   ++chance;
-  if (chance % 2 == 1) turn(square.target.id, player1);
-  else turn(square.target.id, player2);
+  if (chance % 2 == 1) {
+    document.querySelector(".turn").innerText = "Player-2 Turn";
+    turn(square.target.id, player1);
+  } else {
+    document.querySelector(".turn").innerText = "Player-1 Turn";
+    turn(square.target.id, player2);
+  }
 }
 
 function turn(blockID, player) {
@@ -42,19 +49,61 @@ function turn(blockID, player) {
   }
   let playWon = checkWin(boardDesc, player);
   if (playWon) playOver(playWon);
+  if (chance == 9 && !playWon) {
+    for (let i = 0; i < 9; i++) {
+      boxes[i].style.backgroundColor = "green";
+      boxes[i].removeEventListener("click", boxClick, false);
+    }
+    declareWinner("Game Tied!");
+  }
   boxes[blockID].removeEventListener("click", boxClick, false);
 }
 
 function checkWin(board, player) {
-  let game = board.reduce((a, e, i) => (e === player ? a.concat(i) : a), []);
-  let playWon = null;
-  for (let [index, won] of winnerComb.entries()) {
-    if (won.every((ele) => game.indexOf(ele) >= 0)) {
-      playWon = { index: index, player: player };
-      break;
+  let playWin = null;
+  let index = -1;
+  let flag = 0;
+  if (board[4] == player) {
+    if (board[0] == player && board[8] == player) {
+      flag = 1;
+      index = 6;
+    }
+    if (board[2] == player && board[6] == player) {
+      flag = 1;
+      index = 7;
+    }
+    if (board[1] == player && board[7] == player) {
+      flag = 1;
+      index = 4;
+    }
+    if (board[3] == player && board[5] == player) {
+      flag = 1;
+      index = 1;
     }
   }
-  return playWon;
+  if (board[0] == player) {
+    if (board[1] == player && board[2] == player) {
+      flag = 1;
+      index = 0;
+    }
+    if (board[3] == player && board[6] == player) {
+      flag = 1;
+      index = 3;
+    }
+  }
+  if (board[8] == player) {
+    if (board[5] == player && board[2] == player) {
+      flag = 1;
+      index = 5;
+    }
+    if (board[6] == player && board[7] == player) {
+      flag = 1;
+      index = 2;
+    }
+  }
+  if (flag == 1) playWin = { index: index, player: player };
+
+  return playWin;
 }
 
 function playOver(playWon) {
@@ -65,4 +114,13 @@ function playOver(playWon) {
   for (let i = 0; i < boxes.length; i++) {
     boxes[i].removeEventListener("click", boxClick, false);
   }
+  declareWinner(
+    playWon.player == player1 ? "Player-1 Wins!" : "Player-2 Wins!"
+  );
+}
+
+function declareWinner(name) {
+  document.querySelector(".turn").innerText = "";
+  document.querySelector(".endgame").style.display = "block";
+  document.querySelector(".endgame .text").innerText = name;
 }
